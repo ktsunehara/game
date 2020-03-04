4584ff21f71e8a2dc5d9973e55852d5aca21927b
常原清隆 <tsuneharakiyotaka@tsuneharakiyotakanoMacBook-Air.local>
2020-03-04 15:26:27 +0900

    add brave lose

--- brave.rb
+++ brave.rb
@@ -1,12 +1,11 @@
 require './character'
-require './dragon'
 
 
 class Brave < Character
 
   def attack(dragon)
         damage = @offence - dragon.defence
-        dragon.hp = dragon.hp -= damage
+        dragon.hp -= damage
       puts "#{@name}の攻撃"
       puts "#{dragon.name}に#{damage}のダメージを与えた！"
   end
--- dragon.rb
+++ dragon.rb
@@ -1,12 +1,11 @@
 require './character'
-require './brave'
 
 class Dragon < Character
-  
+
   def attack(brave)
     puts "#{@name}の攻撃"
     damage = @offence - brave.defence
-    brave.hp = brave.hp -= damage
+    brave.hp -= damage
     puts "#{brave.name}は#{damage}のダメージを受けた！!"
   end
 end
